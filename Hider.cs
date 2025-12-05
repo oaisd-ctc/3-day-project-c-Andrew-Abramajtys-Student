a60c42245b7eb0a71b3ea29243d9043da19de2d3
Andrew Abramajtys <anab639@student.oaisd.org>
2025-12-05 19:19:41 +0000

    added spot randomizer

--- Hider.cs
+++ Hider.cs
@@ -1,8 +1,20 @@
 namespace HideNSeek;
 public class Hider
 {
+    private int spotChoice;
     public Hider()
     {
-        
+        this.spotChoice = spotChoice;
+    }
+    public void ResetSpot()
+    {
+        Random random = new Random();
+        int rand1 = random.Next(1, 8);
+        int randNumber = rand1;
+        spotChoice = randNumber;
+    }
+    public void TestRandom()
+    {
+        Console.WriteLine("Hider is in spot " + spotChoice);
     }
 }
