e0cbbbcbc2ac2339a74d0ed0fb759c35e6a6d1f6
Andrew Abramajtys <anab639@student.oaisd.org>
2025-12-08 18:14:54 +0000

    Combined hider randomization with guessing code

---
+++ Spots.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using HideNSeek;
+
+
+public class Spots
+{
+    public static void Main(string[] args)
+    {
+        hidingSpots();
+    }
+
+    public static void hidingSpots()
+    {
+        Hider hider = new Hider();
+        hider.ResetSpot();
+
+        string[] spots = { "Cabinet", "Fridge", "Closet", "Bed", "Desk", "Laundry", "Attic" };
+        int attempts = 0;
+
+        while (attempts < 3)
+        {
+            Console.WriteLine("Pick a spot you think they might be hiding in. The Cabinet, Fridge, Closet, Bed, Desk, Laundry, or Attic.");
+            string? spot = Console.ReadLine();
+            
+            if (spot == "end")
+            {
+                Environment.Exit(0);
+            }
+
+            if (string.IsNullOrWhiteSpace(spot))
+            {
+                Console.WriteLine("Please enter a spot.");
+                continue;
+            }
+
+            if (!spots.Contains(spot, StringComparer.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Thats not a hiding spot try looking somewhere else.");
+                continue;
+            }
+
+            int spotChoice = hider.GetSpotChoice();
+            string hiderSpot = spots[spotChoice];
+
+            if (string.Equals(spot, hiderSpot, StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("You found them congratulations, on to the next round.");
+                break;
+            }
+            else
+            {
+                attempts++;
+                if (attempts == 3)
+                {
+                    Console.WriteLine("Looks like they win, Try Again.");
+                    break;
+                }
+                Console.WriteLine("You guessed wrong, try again.");
+                Console.WriteLine();
+            }
+        }
+    }
+}
